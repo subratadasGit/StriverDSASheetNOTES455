770f5a8bc27e0911219836e022ae77f1356fb6bf
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-03-04 15:42:33 +0530

    Add files via upload

---
+++ Recursion/Day2Java/Day2java02.java
@@ -0,0 +1,29 @@
+
+//Print numbers 1 to N
+
+package Day2Java;
+
+import java.util.Scanner;
+
+public class Day2java02 {
+
+    static void f(int count , int n ){
+        if(count==n){
+            return ;
+        }
+        else{
+      System.out.println("Subrata Das");
+      f(count+1, n);
+    }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Range to print the Numbers");
+        int n = sc.nextInt();
+
+        f(0,n);
+        sc.close();
+    }
+    
+}
