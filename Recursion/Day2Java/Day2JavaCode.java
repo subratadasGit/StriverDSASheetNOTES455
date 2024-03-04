ecb46fc47cc00e215f51f9cd4f06d56311cb15ce
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-03-04 15:34:30 +0530

    Day2JavaCode.java

--- Recursion/Day2Java/Day2JavaCode.java
+++ Recursion/Day2Java/Day2JavaCode.java
@@ -1,24 +1,27 @@
-// package Day2Java;
+package Day2Java;
 
-// // print name 5 times 
-// public class Day2JavaCode {
-// static int count =0;
-//     static void f(){
-//         if(count==5){
-//             return ;
-//         }
-//         else{
-//             System.out.println("Subrata Das");
-//             count++;
-//             f();
-//         }
-//     }
+// print name 5 times 
+public class Day2JavaCode {
+static int count =0;
+    static void f(){
+        if(count==5){
+            return ;
+        }
+        else{
+            System.out.println("Subrata Das");
+            count++;
+            f();
+        }
+    }
 
-//     public static void main(String[] args) {
-//         f();
-//     }
+    public static void main(String[] args) {
+        f();
+    }
     
-// }
+}
+
+
+----------------------------------------------------------------->AnotherMethod
 
 package Day2Java;
 
